1f28c46a5453d0e83d3d0fde536c47699789934c
tejasgowda104 <61399408+tejasgowda104@users.noreply.github.com>
2020-02-25 16:20:10 +0530

    Add files via upload

---
+++ ten.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int age=15;
+if (age <=18 && age>=13)
+     {
+     printf("you are teenager");  
+     }
+     else
+     {
+     printf("you are not teenager");
+     }
+}
