0ca0c54c9fb3593970bb40343b7ec65b53934331
tejasgowda104 <61399408+tejasgowda104@users.noreply.github.com>
2020-03-02 10:29:00 +0530

    Add files via upload

---
+++ age.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int age;
+printf("enter age");
+scanf("%d",&age);
+if (age>=13&&age<=21)
+{
+printf("you are teenager");
+}
+else
+{
+printf("you are not a teenager");
+}
+}
