2809ff55433504aef6b058ad9f9bb5509ec3e584
tejasgowda104 <61399408+tejasgowda104@users.noreply.github.com>
2020-03-09 08:49:44 +0530

    Add files via upload

---
+++ salary.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+float g,b,h,d;
+printf("enter the value of b\n");
+scanf("%f",&b);
+h=0.2*b;
+printf("h=%f",h);
+d=0.4*(b);
+printf("d=%f",d);
+g=b+h+d;
+printf("g=%f",g);
+}
